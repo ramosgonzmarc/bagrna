acf3e1ce7262a8adf5f0933a0221b0b6168fed07
ramosgonzmarc <ramosgonzmarc@gmail.com>
2020-11-17 16:31:09 +0100

    Reading data and index

--- bagrna.sh
+++ bagrna.sh
@@ -1,6 +1,7 @@
-#! /usr/bin/bash
+#! /bin/bash
 
-if [ $# -eq 0 || $# -gt 1 ]
+##Help message:
+if [ $# -ne 1 ]
 then
   echo "The number of arguments is: $#"
   echo "Usage: bash bagrna.sh <params_file>"
@@ -10,5 +11,64 @@ then
 fi
 
 PARAMS=$1
+
+echo "==================="
+echo "LOADING PARAMETERS:"
+echo "==================="
+
+WD=$(grep working_directory: $PARAMS | awk '{print($2)}')
+echo "Working directory = $WD"
 EXP=$(grep experiment_name: $PARAMS | awk '{ print($2)}')
 echo "Experiment name= $EXP"
+NUMSAMPLES=$(grep number_samples: $PARAMS | awk '{ print($2)}')
+echo "Number of samples = $NUMSAMPLES"
+
+GENOME=$(grep path_genome: $PARAMS | awk '{print($2)}')
+echo "Reference genome = $GENOME"
+ANNOTATION=$(grep path_annotation: $PARAMS | awk '{print($2)}')
+echo "Annotation= $ANNOTATION"
+SAMPLES=()
+i=0
+while [ $i -lt $NUMSAMPLES ]
+do
+	j=$(($i + 1))
+        SAMPLES[$i]=$(grep path_sample_$j: $PARAMS | awk '{print($2)}')
+        ((i++))
+done
+
+echo "Samples="
+echo "${SAMPLES[@]}"
+
+##Generating work space
+echo "==============="
+
+cd $WD
+mkdir $EXP
+cd $EXP
+mkdir genome annotation results samples
+
+cd genome
+cp $GENOME genome.fa
+cd ../annotation
+cp $ANNOTATION annotation.gtf
+
+##Generating reference genome index
+extract_splice_sites.py annotation.gtf > annot_splice.ss
+extract_exons.py annotation.gtf > annot_exons.exon
+
+cd ../genome
+hisat2-build --ss ../annotation/annot_splice.ss --exon ../annotation/annot_exons.exon genome.fa index
+
+cd ../samples
+i=1
+while [ $i -le $NUMSAMPLES ]
+do
+	mkdir sample_$i
+	cd sample_$i
+	j=$(($i - 1))
+	cp ${SAMPLES[$j]} sample_$i.fq.gz
+	cd ..
+	((i++))
+done
+
+
